0eabae4e09f1e20ee3c8f9873e3311ad8da2b994
shangpguo2 <72008902+shangpguo2@users.noreply.github.com>
2023-03-29 12:15:21 +0800

    change uuid

--- app/src/main/java/com/fyp/cityulogin/util/BluetoothUUID.java
+++ app/src/main/java/com/fyp/cityulogin/util/BluetoothUUID.java
@@ -3,7 +3,7 @@ package com.fyp.cityulogin.util;
 import java.util.UUID;
 
 public class BluetoothUUID {
-    public static final UUID BLE_USER_DATA  = UUID.fromString("5e37a890-3cfd-4fac-9760-39538679fe39");
-    public static final UUID CHAR_EID       = UUID.fromString("15dc4cd9-5c8c-4ca0-926b-fcd09353540c");
-    public static final UUID CHAR_PASSWORD  = UUID.fromString("6ba6c16c-3a62-403c-b163-22183dcd084f");
+    public static final UUID BLE_USER_DATA  = UUID.fromString("00001234-3cfd-4fac-9760-39538679fe39");
+    public static final UUID CHAR_EID       = UUID.fromString("00001234-5c8c-4ca0-926b-fcd09353540c");
+    public static final UUID CHAR_PASSWORD  = UUID.fromString("00001234-3a62-403c-b163-22183dcd084f");
 }
